00c8f5b25b73c70fadcba78c3a0e61115111180b
Peter Pal Piroska <peter.piroska@visma.com>
2021-09-29 16:49:50 +0300

    Update tf.sh
    Co-authored-by: Alban Fonrouge <alban.fonrouge@visma.com>

--- tf.sh
+++ tf.sh
@@ -230,7 +230,7 @@ if [ -n "$TF_VERSION" ]; then
         echo "Working with terraform v0.15 ..."
         TERRAFORM_BIN="terraform-0.15"
     elif [ $TF_VERSION = "1.0" ]; then
-        echo "Working with terraform v1.0.7 ..."
+        echo "Working with terraform v1.0 ..."
         TERRAFORM_BIN="terraform-1.0"
     else
         echo "The used version from terraform.version file is wrong or missing!"
